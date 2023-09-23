c7f191ef1212ed832ac1a26f66d8cd2b8d15435a
migos143 <glenranido11@gmail.com>
2023-09-23 20:19:34 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Homeaid</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+    
+</body>
+</html>
